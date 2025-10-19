abdfc27428f144f3030506bbed4af490d049f9d3
MYKOLA YAKYMCHUK <m.yakymchuk5@gmail.com>
2025-10-19 18:37:57 +0200

    Implement utility methods for StreamPractice and define CandidateValidator logic

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,33 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
     //write your code here
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final int MIN_AGE = 35;
+    private static final int REQUIRED_YEARS_IN_UKRAINE = 10;
+
+    public boolean test(Candidate candidat) {
+        if (candidat == null) {
+            return false;
+        }
+        boolean isValid = candidat.getAge() >= MIN_AGE
+                && candidat.isAllowedToVote()
+                && candidat.getNationality().equals(REQUIRED_NATIONALITY);
+        if (!isValid) {
+            return false;
+        }
+        String[] years = candidat.getPeriodsInUkr().split("-");
+        if (years.length != 2) {
+            return false;
+        }
+        int startYear = Integer.parseInt(years[0]);
+        int endYear = Integer.parseInt(years[1]);
+        int yearsInUkraine = endYear - startYear;
+
+        return yearsInUkraine >= REQUIRED_YEARS_IN_UKRAINE;
+
+    }
 }
